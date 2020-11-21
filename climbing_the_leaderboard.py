e095085493959ae71585f25b47fbe518570e52fa
shangar <sharanshangar@gmail.com>
2020-11-20 20:33:03 -0500

    not big brain enough gonna do a diff problem, solution on here tho :(

--- climbing_the_leaderboard.py
+++ climbing_the_leaderboard.py
@@ -4,6 +4,9 @@ import random
 import re
 import sys
 
+
+#My Solutions:
+
 def climbingLeaderboard(ranked, player):
     pscores = []
     
@@ -16,4 +19,45 @@ def climbingLeaderboard(ranked, player):
 
 
 
-print(climbingLeaderboard([100,100,50,40,40,20,10], [5,25,50,120]))
+
+def climbingLeaderboard(ranked, player):
+    pscores = []
+
+    ranked = list(dict.fromkeys(sorted(ranked, reverse=True)))
+
+    for i in range(len(player)):
+    	if (player[i] < ranked[len(ranked) - 1]):
+    		pscores.append(len(ranked) + 1)
+    		ranked.append(player[i])
+    	else:
+    		j = 0
+    		while j < (len(ranked)):
+    			if (player[i] == ranked[j]):
+    				pscores.append(j+1)
+    				j = len(ranked) + 1
+    			elif (player[i] > ranked[j]):
+    				ranked.insert(j, player[i])
+    				pscores.append(j+1)
+    				j = len(ranked) + 1
+    			j += 1
+    return pscores
+
+#Correct Solution (found at https://livecodestream.dev/challenge/climbing-the-leaderboard/)
+
+def climbingLeaderboard(ranked, player):
+    # get the unique ranks sorted descending
+    scores = sorted(list(set(ranked)), reverse=True)
+    player_ranks = []
+    for score in player:
+        while scores and score >= scores[-1]:
+            scores.pop()
+        player_ranks.append(len(scores) + 1)
+
+    return player_ranks
+
+
+
+ranked = [100,90,90,80,75,60]
+player = [50,65,77,90,102]
+print(climbingLeaderboard(ranked, player))
+
